4f086bfe3e352de288ce394b6f96b93ecf973e12
MarenWoodruff <wren403@gmail.com>
2017-01-05 08:48:19 -0500

    working on posts controller

--- app.js
+++ app.js
@@ -1,13 +1,74 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']); // any time you add an external module, you need to include it as a dependency
+
+app.factory('posts', [function(){
+  // service body
+  var o = {
+    posts: []
+  };
+  return o;
+}])
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
+  '$scope', 
+  'posts',
+  function($scope, posts){
+    $scope.test = 'Hello world!';
 
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 3},
-    {title: 'post 3', upvotes: 2}
-  ];
-}]);
+    $scope.posts = posts.posts;
+    // $scope.posts = [
+    //   {title: 'post 1', upvotes: 5},
+    //   {title: 'post 2', upvotes: 3},
+    //   {title: 'post 3', upvotes: 2}
+    // ];
+
+    $scope.addPost = function() {
+      if ($scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
+      $scope.title = ''; // clearing out the title after it is submitted
+      $scope.link = '';
+    }
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
+  }
+]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id]; // use the index as the id/$stateParams will retrieve the id from the url and grab the correct post
+  }
+]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'MainCtrl'
+      });
+
+      .state('posts', {
+        url: '/posts/{id}', // brackets make id a route parameter, that is available to the controller
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }
+]);
